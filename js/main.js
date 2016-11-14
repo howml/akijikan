9dacc682dd88c3103aaef4a14699bd49b4d09c13
Taisuke Fukuno <fukuno@tpc0135.internal.jig.jp>
2016-11-14 16:52:42 +0900

    ランダムオープンデータ表示

--- js/main.js
+++ js/main.js
@@ -25,8 +25,9 @@ var addItem = function(s, img, list) {
 		span2.appendChild(img);
 		*/
 		span2.style = "text-align: center; line-height: 10rem; display: block; float: right; margin-top: 2.5rem; width: 5rem; height: 5rem;";
-		span2.style.backgroundImage = "url(img/image.png)";
-		span2.style.backgroundSize = "5rem auto";
+		//		span2.style.backgroundImage = "url(img/image.png)";
+		span2.style.backgroundImage = "url(" + img + ")";
+		span2.style.backgroundSize = "auto 5rem";
 		span2.style.backgroundPosition = "center center";
 	} else {
 		span2.innerHTML = "<i class=material-icons>broken_image</i>";
@@ -42,7 +43,46 @@ var addItem = function(s, img, list) {
 	}
 };
 
+var getCulturalPropertyWithGeo = function(size, callback) {
+	var q = f2s(function() {/*
+		prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+		prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+		select ?name ?desc ?url ?img ?lat ?lng {
+			?s <http://schema.org/image> ?img;
+				rdfs:label ?name;
+				geo:lat ?lat;
+				geo:long ?lng.
+			optional { ?s <http://schema.org/description> ?desc }
+			optional { ?s <http://schema.org/url> ?link }
+		} order by rand() limit $SIZE$
+	*/});
+	q = q.replace(/\$SIZE\$/g, size);
+	
+	var baseurl = "http://sparql.odp.jig.jp/data/sparql";
+	querySPARQL(baseurl, q, function(data) {
+		callback(toList(data));
+	});
+};
+var toList = function(data) {
+	var items = data.results.bindings;
+	var list = [];
+	for (var uri in items) {
+		var it = items[uri];
+		var d = {};
+		for (var v in it) {
+			d[v] = it[v].value;
+		}
+		list.push(d);
+	}
+	return list;
+};
+
 $(function() {
-	addItem("あいう", null, [ "a", "b" ]);
-	addItem("がぞうあり", true, [ "a", "b" ]);
+	getCulturalPropertyWithGeo(12, function(data) {
+		//		dump(data);
+		for (var i = 0; i < data.length; i++) {
+			var d = data[i];
+			addItem(d.name.substring(0, 6), d.img, [ d.name, d.desc, d.lat + "," + d.lng ]);
+		}
+	});
 });
